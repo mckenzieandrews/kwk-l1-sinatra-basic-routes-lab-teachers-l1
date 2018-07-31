e45d7a700b181048d7d15adeae0b809722dd8fcf
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-07-31 16:59:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello World"
+  end 
+  
+  get '/name' do
+    "My name is McKenzie"
+  end
+  
+  get '/hometown' do
+    "My hometown is Nashville."
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is A Drop in the Ocean"
+  end 
+  
 end
